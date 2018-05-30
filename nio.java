c4024a9eb7ecee82cc81d09ae137f1b22ce7c3a0
manideep-cs <39744535+manideep-cs@users.noreply.github.com>
2018-05-30 09:04:50 +0530

    Add files via upload

---
+++ nio.java
@@ -0,0 +1,15 @@
+
+
+public class nio {
+	public static void main(String args[]) {
+		
+		while(true){
+
+                            System.out.print("0110");
+		
+                            System.out.print("1010");
+
+                            }
+	}
+
+     }
